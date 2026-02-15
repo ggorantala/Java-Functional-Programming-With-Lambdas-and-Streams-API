23621c8187f439a123d822b21077bac591f2e640
Gopi Gorantala <gopishiva001@outlook.com>
2026-02-15 08:52:59 +0100

    feature: Addded fee more examples to streams-api

---
+++ dev/ggorantala/chapters/functionalinterfaces/exercises/problemtwo/Solution.java
@@ -0,0 +1,21 @@
+package dev.ggorantala.chapters.functionalinterfaces.exercises.problemtwo;
+/*
+ * This code is part of the course "Master Java 8(11/17) Functional Programming With Lambdas And Streams API"
+ * for https://ggorantala.dev.
+ *
+ * Copyright (C) 2023 by Gopi Gorantala.
+ *
+ * @author ggorantala
+ * Date: 07/05/23
+ */
+
+public class Solution {
+    public static void main(String[] args) {
+        PowerOfTwoFI helper = n -> n != 0 && (n & (n - 1)) == 0; // lambda expression
+
+        System.out.println(helper.isPowerOfTwo(4));
+        System.out.println(helper.isPowerOfTwo(3));
+        System.out.println(helper.isPowerOfTwo(10));
+        System.out.println(helper.isPowerOfTwo(16));
+    }
+}
---
+++ dev/ggorantala/chapters/streams/create/StreamCollectionExample.java
@@ -0,0 +1,24 @@
+package dev.ggorantala.chapters.streams.create;
+/*
+ * This code is part of the course "Master Java 8(11/17) Functional Programming With Lambdas And Streams API"
+ * for https://ggorantala.dev.
+ *
+ * Copyright (C) 2023 by Gopi Gorantala.
+ *
+ * @author ggorantala
+ * Date: 07/05/23
+ */
+
+import dev.ggorantala.Constants;
+
+import java.util.stream.Stream;
+
+public class StreamCollectionExample {
+    public static void main(String[] args) {
+
+        Stream<String> stream = Constants.STRING_LIST.stream();
+
+        // Perform operations on the stream
+        stream.forEach(System.out::println);
+    }
+}
